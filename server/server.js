0abc518f1fbc2bc0d6c3fbebdd40736a377b666a
Aleksey Brailko <alexbrailko@gmail.com>
2017-11-28 13:26:57 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+
+app.use(express.static(publicPath));
+  
+app.listen(port, () => {
+    console.log('server is up');
+}); 
