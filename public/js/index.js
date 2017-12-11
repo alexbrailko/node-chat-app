18569a17ce269c73df5a3add92c37ae8aa05cbb6
Aleksey Brailko <alexbrailko@gmail.com>
2017-12-11 15:08:09 +0200

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'dfgd',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function() {
-
+        messageTextbox.val('');
     });
 });
 
@@ -42,12 +44,18 @@ locationButton.on('click', function() {
         return alert('Geolocation not supported');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send location');
+
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
+
         alert('Unable to fetch locaction');
     });
 });
