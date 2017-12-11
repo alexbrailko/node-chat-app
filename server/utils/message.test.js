a2f1dccfe88bedf09b95df589fe90b784880c17f
Aleksey Brailko <alexbrailko@gmail.com>
2017-12-11 12:52:09 +0200

    add geolocation support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate a correct message object', () => {
@@ -9,5 +9,18 @@ describe('generateMessage', () => {
 
         expect(message.createdAt).to.be.a('number');
         expect(message).to.include({ from, text});
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate a correct location object', () => {
+        const from = 'Alex';
+        const lat = '15';
+        const lng = '19';
+        const url = 'https://www.google.com/maps?q=15,19';
+        const message = generateLocationMessage(from, lat, lng);
+
+        expect(message.createdAt).to.be.a('number');
+        expect(message).to.include({ from, url });
     });
 });
